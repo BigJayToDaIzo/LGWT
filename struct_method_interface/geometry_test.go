255ad795b5b63defb27b626ec08dc3da68dbd380
Joseph Myers <josephlmyers@gmail.com>
2024-07-12 21:18:37 -0500

    test suite refactor

--- struct_method_interface/geometry_test.go
+++ struct_method_interface/geometry_test.go
@@ -9,19 +9,23 @@ func TestPerimeter(t *testing.T) {
 	perimeterTests := []struct {
 		shape Shape
 		want  float64
+		desc  string
 	}{
-		{&Rectangle{10.0, 4.0}, 28.0},
-		{&Circle{10}, 10 * 2 * math.Pi},
+		{&Rectangle{10.0, 4.0}, 28.0, "rect 10x4"},
+		{&Circle{10}, 10 * 2 * math.Pi, "circle 10"},
+		{&Triangle{10, 3}, 23.440307, "triangle 10x3"},
 	}
 	checkPerimeter := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Perimeter()
-		if got != want {
+		if math.Abs(got-want) > 0.00001 {
 			t.Errorf("got %.8g want %.8g", got, want)
 		}
 	}
 	for _, tt := range perimeterTests {
-		checkPerimeter(t, tt.shape, tt.want)
+		t.Run(tt.desc, func(t *testing.T) {
+			checkPerimeter(t, tt.shape, tt.want)
+		})
 	}
 }
 
@@ -29,18 +33,22 @@ func TestArea(t *testing.T) {
 	areaTests := []struct {
 		shape Shape
 		want  float64
+		desc  string
 	}{
-		{&Rectangle{10.0, 5.0}, 50.0},
-		{&Circle{10}, 10 * 10 * math.Pi},
+		{&Rectangle{10.0, 5.0}, 50.0, "rect 10x5"},
+		{&Circle{10}, 10 * 10 * math.Pi, "circle 10"},
+		{&Triangle{10, 3}, 15, "triangle 10x3"},
 	}
 	checkArea := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
 		got := shape.Area()
-		if math.Abs(got-want) > 0.00000001 {
+		if math.Abs(got-want) > 0.00001 {
 			t.Errorf("got %.8g want %.8g", got, want)
 		}
 	}
 	for _, tt := range areaTests {
-		checkArea(t, tt.shape, tt.want)
+		t.Run(tt.desc, func(t *testing.T) {
+			checkArea(t, tt.shape, tt.want)
+		})
 	}
 }
