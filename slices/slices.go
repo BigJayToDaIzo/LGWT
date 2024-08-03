2e6b1838fdb4555b8e23ad2b2a1e4544d34bddd9
Joseph Myers <josephlmyers@gmail.com>
2024-08-02 21:59:56 -0500

    Reduce and CheckBalance working pre refactor

--- slices/slices.go
+++ slices/slices.go
@@ -1,5 +1,24 @@
 package slices
 
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
+}
+
+func BalanceFor(tr []Transaction, name string) float64 {
+	var balance float64
+	for _, trans := range tr {
+		if trans.From == name {
+			balance -= trans.Sum
+		}
+		if trans.To == name {
+			balance += trans.Sum
+		}
+	}
+	return balance
+}
+
 func SumSlice(s []int) int {
 	sum := 0
 	for _, v := range s {
@@ -8,26 +27,27 @@ func SumSlice(s []int) int {
 	return sum
 }
 
-func SumSlices(sums ...[]int) int {
-	sum := 0
-	for _, s := range sums {
-		if len(s) == 0 {
-			sum += 0
-		} else {
-			sum += SumSlice(s)
-		}
+func Reduce[A any](collection []A, f func(A, A) A, initValue A) A {
+	var result = initValue
+	for _, x := range collection {
+		result = f(result, x)
 	}
-	return sum
+	return result
 }
 
-func SumSliceTails(sums ...[]int) int {
-	tailSum := 0
-	for _, s := range sums {
-		if len(s) == 0 {
-			tailSum += 0
+func SumSlices(sums []int) int {
+	add := func(acc, x int) int { return acc + x }
+	return Reduce(sums, add, 0)
+}
+
+func SumSliceTails(sums ...[]int) []int {
+	sumTail := func(acc, x []int) []int {
+		if len(x) == 0 {
+			return append(acc, 0)
 		} else {
-			tailSum += s[len(s)-1]
+			tail := x[1:]
+			return append(acc, SumSlices(tail))
 		}
 	}
-	return tailSum
+	return Reduce(sums, sumTail, []int{})
 }
--- slices/slices_test.go
+++ slices/slices_test.go
@@ -1,61 +1,92 @@
 package slices
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestSumSlice(t *testing.T) {
-	sumSliceTests := []struct {
-		Slice []int
-		want  int
-		desc  string
-	}{
-		{[]int{1, 2, 3, 4, 5}, 15, "sum of 1 to 5"},
-		{[]int{2, 3, 4, 5}, 14, "sum of 2 to 5"},
-		{[]int{}, 0, "empty slice"},
-	}
-	for _, tt := range sumSliceTests {
-		fmt.Println(tt.desc)
-		assertCorrectness(t, tt.want, SumSlice(tt.Slice))
-	}
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+		assertCorrectness(t, Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+		assertCorrectness(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
 }
 
-func TestSumSlices(t *testing.T) {
-	sumSlicesTests := []struct {
-		Slices [][]int
-		want   int
-		desc   string
-	}{
-		{[][]int{{1, 2, 3}, {4, 5, 6}}, 21, "sum of 1 to 6 (2 slices)"},
-		{[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, 45, "sum of 1 to 9 (3 slices)"},
-		{[][]int{{}}, 0, "empty slice"},
+func TestBadBank(t *testing.T) {
+	trans := []Transaction{
+		{
+			From: "Chris",
+			To:   "Riya",
+			Sum:  100,
+		},
+		{
+			From: "Adil",
+			To:   "Chris",
+			Sum:  25,
+		},
 	}
-	for _, tt := range sumSlicesTests {
-		fmt.Println(tt.desc)
-		assertCorrectness(t, tt.want, SumSlices(tt.Slices...))
-	}
+	assertCorrectness(t, BalanceFor(trans, "Riya"), 100)
+	assertCorrectness(t, BalanceFor(trans, "Chris"), -75)
+	assertCorrectness(t, BalanceFor(trans, "Adil"), -25)
 }
 
-func TestSumSliceTails(t *testing.T) {
-	testSumSliceTails := []struct {
-		Slices [][]int
-		want   int
-		desc   string
-	}{
-		{[][]int{{1, 2, 3}, {4, 5, 6}}, 9, "sum of slice tails 3 and 6"},
-		{[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, 18, "sum of slice tails 3, 6, and 9"},
-		{[][]int{{}}, 0, "empty slice"},
-	}
-	for _, tt := range testSumSliceTails {
-		fmt.Println(tt.desc)
-		assertCorrectness(t, tt.want, SumSliceTails(tt.Slices...))
-	}
-}
-
-func assertCorrectness(t *testing.T, expected, actual int) {
+//	func TestSumSlice(t *testing.T) {
+//		sumSliceTests := []struct {
+//			Slice []int
+//			want  int
+//			desc  string
+//		}{
+//			{[]int{1, 2, 3, 4, 5}, 15, "sum of 1 to 5"},
+//			{[]int{2, 3, 4, 5}, 14, "sum of 2 to 5"},
+//			{[]int{}, 0, "empty slice"},
+//		}
+//		for _, tt := range sumSliceTests {
+//			fmt.Println(tt.desc)
+//			assertCorrectness(t, tt.want, SumSlice(tt.Slice))
+//		}
+//	}
+//
+//	func TestSumSlices(t *testing.T) {
+//		sumSlicesTests := []struct {
+//			Slices [][]int
+//			want   int
+//			desc   string
+//		}{
+//			{[][]int{{1, 2, 3}, {4, 5, 6}}, 21, "sum of 1 to 6 (2 slices)"},
+//			{[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, 45, "sum of 1 to 9 (3 slices)"},
+//			{[][]int{{}}, 0, "empty slice"},
+//		}
+//		for _, tt := range sumSlicesTests {
+//			fmt.Println(tt.desc)
+//			assertCorrectness(t, tt.want, SumSlices(tt.Slices...))
+//		}
+//	}
+//
+//	func TestSumSliceTails(t *testing.T) {
+//		testSumSliceTails := []struct {
+//			Slices [][]int
+//			want   int
+//			desc   string
+//		}{
+//			{[][]int{{1, 2, 3}, {4, 5, 6}}, 9, "sum of slice tails 3 and 6"},
+//			{[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}, 18, "sum of slice tails 3, 6, and 9"},
+//			{[][]int{{}}, 0, "empty slice"},
+//		}
+//		for _, tt := range testSumSliceTails {
+//			fmt.Println(tt.desc)
+//			assertCorrectness(t, tt.want, SumSliceTails(tt.Slices...))
+//		}
+//	}
+func assertCorrectness[A comparable](t *testing.T, expected, actual A) {
 	if actual != expected {
 		t.Helper()
-		t.Errorf("Expected %d, got %d", expected, actual)
+		t.Errorf("Expected %v, got %v", expected, actual)
 	}
 }
