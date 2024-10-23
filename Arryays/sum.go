dab82fc06ad18619a9336b722d9d810b77b4ac41
Joseph Myers <josephlmyers@gmail.com>
2024-10-23 14:11:26 -0500

    slices wrap up

--- Arryays/sum.go
+++ Arryays/sum.go
@@ -7,3 +7,26 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(numsToSum ...[]int) []int {
+	lenOfNums := len(numsToSum)
+	sums := make([]int, lenOfNums)
+	for i, n := range numsToSum {
+		sums[i] = Sum(n)
+	}
+	return sums
+}
+
+// clips head and sums the rest
+func SumAllTails(tailsToSum ...[]int) []int {
+	var sums []int
+	for _, n := range tailsToSum {
+		if len(n) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := n[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
--- Arryays/sum_test.go
+++ Arryays/sum_test.go
@@ -1,12 +1,69 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := []int{1, 2, 3, 4, 5}
-	got := Sum(numbers)
-	want := 15
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
+
+}
+
+// func checkEquality(t *testing.T, got, want []int) {
+// 	t.Helper()
+// 	if !reflect.DeepEqual(got, want) {
+// 		t.Errorf("got %v want %v", got, want)
+// 	}
+// }
+
+func TestSumAll(t *testing.T) {
+	checkEquality := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
 	}
+	t.Run("sum slices and return a slice of their sums", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{3, 4})
+		want := []int{3, 7}
+		checkEquality(t, got, want)
+	})
+	t.Run("sum 3 slices of 3", func(t *testing.T) {
+		got := SumAll([]int{1, 2, 3}, []int{4, 5, 6}, []int{7, 8, 9})
+		want := []int{6, 15, 24}
+		checkEquality(t, got, want)
+	})
+	t.Run("sum slices of varying len", func(t *testing.T) {
+		got := SumAll([]int{1, 2, 3}, []int{4, 5, 6, 7}, []int{8, 9})
+		want := []int{6, 22, 17}
+		checkEquality(t, got, want)
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	// tighten up scope on helper funcs to keep api privacy strong
+	checkEquality := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+	t.Run("sum tails of slices and return a slice of their sums", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{3, 4})
+		want := []int{2, 4}
+		checkEquality(t, got, want)
+	})
+	t.Run("returns []int{0} for size 0 array or nil", func(t *testing.T) {
+		got := SumAllTails(nil)
+		want := []int{0}
+		checkEquality(t, got, want)
+	})
 }
