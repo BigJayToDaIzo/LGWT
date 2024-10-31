c4d3ae8b027ca7e30d02c39d8a0efb6d65d6bfe5
Joseph Myers <josephlmyers@gmail.com>
2024-10-31 11:35:59 -0500

    Prop based tests are a wild ride

--- Props/props.go
+++ Props/props.go
@@ -4,121 +4,120 @@ import (
 	"strings"
 )
 
-func ConvertToRomanBigEndian(arabic int) string {
-	var res strings.Builder
-	for arabic > 0 {
-		switch {
-		case arabic/1000 >= 1:
-			for i := 0; i < arabic/1000; i++ {
-				res.WriteString("M")
-				arabic -= 1000
-			}
-		case arabic/900 >= 1:
-			res.WriteString("CM")
-			arabic -= 900
-		case arabic/500 >= 1:
-			for i := 0; i < arabic/500; i++ {
-				res.WriteString("D")
-				arabic -= 500
-			}
-		case arabic/400 >= 1:
-			res.WriteString("CD")
-			arabic -= 400
-		case arabic/100 >= 1:
-			res.WriteString("C")
-			arabic -= 100
-		case arabic/90 >= 1:
-			res.WriteString("XC")
-			arabic -= 90
-		case arabic/50 >= 1:
-			res.WriteString("L")
-			arabic -= 50
-		case arabic/40 >= 1:
-			res.WriteString("XL")
-			arabic -= 40
-		case arabic/10 >= 1:
-			res.WriteString("X")
-			arabic -= 10
-		case arabic/9 >= 1:
-			res.WriteString("IX")
-			arabic -= 9
-		case arabic/5 >= 1:
-			res.WriteString("V")
-			arabic -= 5
-		case arabic/4 >= 1:
-			res.WriteString("IV")
-			arabic -= 4
-		default:
-			for i := 0; i < arabic; i++ {
-				res.WriteString("I")
-				arabic--
-			}
+// abstracting of of arabic to roman symbol
+// to a struct and ordered slice killed the
+// smelly switch statement
+type RomanNumeral struct {
+	Value  uint16
+	Symbol string
+}
+
+var allRomanSymbols = []RomanNumeral{
+	{1000, "M"},
+	{900, "CM"},
+	{500, "D"},
+	{400, "CD"},
+	{100, "C"},
+	{90, "XC"},
+	{50, "L"},
+	{40, "XL"},
+	{10, "X"},
+	{9, "IX"},
+	{5, "V"},
+	{4, "IV"},
+	{1, "I"},
+}
+
+func ConvertToRoman(arabic uint16) string {
+	if arabic == 0 {
+		return ""
+	}
+	var result strings.Builder
+	for _, numeral := range allRomanSymbols {
+		for arabic >= numeral.Value {
+			result.WriteString(numeral.Symbol)
+			arabic -= numeral.Value
 		}
 	}
-	// don't want to return till we run out of arabic
-	return res.String()
+	return result.String()
+}
+
+func ConvertToArabic(roman string) uint16 {
+	if roman == "" {
+		return 0
+	}
+	var result uint16
+	// clip symbols big endian and iterate arabic
+	for _, numeral := range allRomanSymbols {
+		for strings.HasPrefix(roman, numeral.Symbol) {
+			result += numeral.Value
+			roman = strings.TrimPrefix(roman, numeral.Symbol)
+		}
+	}
+	return result
 }
 
 // Romans decreed NO symbol may repeat >3x
-func ConvertToRoman(arabic int) string {
-	// this switch length is a smell
-	// parsing the int with mod may be better
-	var res strings.Builder
-	// base case for recursion?
-	for arabic > 0 {
-		// 5000 and above is ↁ or V^overline
-		// this test will only cover up to 4999
-		switch {
-		case arabic >= 1000:
-			res.WriteString("M")
-			arabic -= 1000
-		case arabic >= 900: // CM
-			res.WriteString("CM")
-			arabic -= 900
-		case arabic >= 500: // D
-			res.WriteString("D")
-			arabic -= 500
-		case arabic >= 400:
-			res.WriteString("CD")
-			arabic -= 400
-		case arabic >= 100: // C
-			res.WriteString("C")
-			arabic -= 100
-		case arabic >= 90:
-			res.WriteString("XC")
-			arabic -= 90
-		case arabic >= 50: // L
-			res.WriteString("L")
-			arabic -= 50
-		case arabic >= 40:
-			res.WriteString("XL")
-			arabic -= 40
-		case arabic >= 10:
-			res.WriteString("X")
-			arabic -= 10
-		case arabic >= 9:
-			res.WriteString("IX")
-			arabic -= 9
-		case arabic >= 5:
-			res.WriteString("V")
-			arabic -= 5
-		case arabic > 3:
-			res.WriteString("IV")
-			arabic -= 4
-		default:
-			res.WriteString(switchOnSmallArabic(arabic))
-			arabic -= arabic
-		}
-	}
-	return res.String()
-}
+// func ConvertToRoman(arabic int) string {
+// 	// this switch length is a smell
+// 	// parsing the int with mod may be better
+// 	var res strings.Builder
+// 	// base case for recursion?
+// 	for arabic > 0 {
+// 		// 4000 and above is denoted by IV^overline
+// 		// 5000 and above is ↁ or V^overline, etc
+// 		// this test will only cover up to 4999
+// 		switch {
+// 		case arabic >= 1000:
+// 			res.WriteString("M")
+// 			arabic -= 1000
+// 		case arabic >= 900: // CM
+// 			res.WriteString("CM")
+// 			arabic -= 900
+// 		case arabic >= 500: // D
+// 			res.WriteString("D")
+// 			arabic -= 500
+// 		case arabic >= 400:
+// 			res.WriteString("CD")
+// 			arabic -= 400
+// 		case arabic >= 100: // C
+// 			res.WriteString("C")
+// 			arabic -= 100
+// 		case arabic >= 90:
+// 			res.WriteString("XC")
+// 			arabic -= 90
+// 		case arabic >= 50: // L
+// 			res.WriteString("L")
+// 			arabic -= 50
+// 		case arabic >= 40:
+// 			res.WriteString("XL")
+// 			arabic -= 40
+// 		case arabic >= 10:
+// 			res.WriteString("X")
+// 			arabic -= 10
+// 		case arabic >= 9:
+// 			res.WriteString("IX")
+// 			arabic -= 9
+// 		case arabic >= 5:
+// 			res.WriteString("V")
+// 			arabic -= 5
+// 		case arabic > 3:
+// 			res.WriteString("IV")
+// 			arabic -= 4
+// 		default:
+// 			res.WriteString(switchOnSmallArabic(arabic))
+// 			arabic -= arabic
+// 		}
+// 	}
+// 	return res.String()
+// }
 
 // Rule for Roman is NO symbol may repeat >3x
-func switchOnSmallArabic(arabic int) string {
-	s := ""
-	for arabic > 0 {
-		s += "I"
-		arabic--
-	}
-	return s
-}
+// func switchOnSmallArabic(arabic int) string {
+// 	s := ""
+// 	for arabic > 0 {
+// 		s += "I"
+// 		arabic--
+// 	}
+// 	return s
+// }
--- Props/props_test.go
+++ Props/props_test.go
@@ -1,151 +1,109 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"log"
+	"testing"
+	"testing/quick"
+)
 
 func TestRomanNumerals(t *testing.T) {
 	cases := []struct {
-		Description string
-		Arabic      int
-		Want        string
+		Arabic uint16
+		Roman  string
 	}{
-		// I implemented for ADDITION
-		{"1 converts to I", 1, "I"},
-		// {"2 converts to II", 2, "II"},
+		// big endian approach works slightly better than
+		// little endian approach, imo
+		// addition through the symbols
+		{Arabic: 1000, Roman: "M"},
+		{Arabic: 2000, Roman: "MM"},
+		{Arabic: 3000, Roman: "MMM"},
+		{Arabic: 3500, Roman: "MMMD"},
+		{Arabic: 3600, Roman: "MMMDC"},
+		{Arabic: 3700, Roman: "MMMDCC"},
+		{Arabic: 3800, Roman: "MMMDCCC"},
+		{Arabic: 3850, Roman: "MMMDCCCL"},
+		// time to bring in subtraction
+		{Arabic: 3900, Roman: "MMMCM"},
+		{Arabic: 3400, Roman: "MMMCD"},
+		// bring in X
+		{Arabic: 3910, Roman: "MMMCMX"},
+		{Arabic: 3920, Roman: "MMMCMXX"},
+		{Arabic: 3930, Roman: "MMMCMXXX"},
+		// X subtraction
+		{Arabic: 3440, Roman: "MMMCDXL"},
+		// bring in V
+		{Arabic: 3935, Roman: "MMMCMXXXV"},
+		// bring in I
+		{Arabic: 3936, Roman: "MMMCMXXXVI"},
+		{Arabic: 3938, Roman: "MMMCMXXXVIII"},
+		{Arabic: 3444, Roman: "MMMCDXLIV"},
+		// did we do it?  Let's PLAY!
+		{Arabic: 3442, Roman: "MMMCDXLII"},
+		{Arabic: 3441, Roman: "MMMCDXLI"},
+		// let is begin again near the lost trail of 3444
+		// now that subtraction is implemented lets lean back into the bigendian thing
+		// and continue toward 3999 on the 4s and 9s
+		{Arabic: 3990, Roman: "MMMCMXC"},
+		{Arabic: 3994, Roman: "MMMCMXCIV"},
+		{Arabic: 3999, Roman: "MMMCMXCIX"},
 
-		// V implemented for SUBTRACTION from at 4
-		{"4 converts to IV", 4, "IV"},
-		// TODO: introduction of new symbol for subtraction is causing breaks
-		// in an terative approach look for pattern in the messy switch
-
-		// V implemented for ADDITION
-		{"5 converts to V", 5, "V"},
-
-		// X implemented for SUBTRACTION from at 9
-		{"9 converts to IX", 9, "IX"},
-		// X implemented for ADDITION at 10
-		{"10 converts to X", 10, "X"},
-
-		// L implemented for SUBTRACTION from at 40
-		{"40 converts to XL", 40, "XL"},
-		// check previous implementation
-		{"44 converts to XLIV", 44, "XLIV"},
-		{"49 converts to XLIX", 49, "XLIX"},
-
-		// L implemented for ADDITION at 50
-		{"50 converts to L", 50, "L"},
-		// check previous implementation
-		{"54 converts to LIV", 54, "LIV"},
-		{"59 converts to LIX", 59, "LIX"},
-		// right before edge of introduction of C for subtraction
-		{"89 converts to LXXXIX", 89, "LXXXIX"},
-
-		// C implemented for SUBTRACTION at 90
-		{"90 converts to XC", 90, "XC"},
-		// check previous breaking points
-		{"94 converts to XCIV", 94, "XCIV"},
-		{"99 converts to XCIX", 99, "XCIX"},
-
-		// C implemented for ADDITION at 100
-		{"100 converts to C", 100, "C"},
-		// check for subtraction regressions due to new digit
-		{"104 converts to CIV", 104, "CIV"},
-		{"109 converts to CIX", 109, "CIX"},
-		// push the envelope
-		{"149 converts to CXLIX", 149, "CXLIX"},
-		{"249 converts to CCXLIX", 249, "CCXLIX"},
-		{"333 converts to CCCXXXIII", 333, "CCCXXXIII"},
-		// nothing should break until we need D for subtraction right?
-		{"399 converts to CCCXCIX", 399, "CCCXCIX"},
-		// we knew needing the D would trip us up here
-		// implements D symbol for SUBTRACTION
-		{"400 converts to CD", 400, "CD"},
-		{"404 converts to CDIV", 404, "CDIV"},
-		{"409 converts to CDIX", 409, "CDIX"},
-		{"433 converts to CDXXXIII", 433, "CDXXXIII"},
-		{"499 converts to CDXCIX", 499, "CDXCIX"},
-
-		// NOW we add the D for ADDITION
-		{"500 converts to D", 500, "D"},
-		{"504 converts to DIV", 504, "DIV"},
-		{"599 converts to DXCIX", 599, "DXCIX"},
-		{"649 converts to DCXLIX", 649, "DCXLIX"},
-		{"899 converts to DCCCXCIX", 899, "DCCCXCIX"},
-
-		// implements M symbol for SUBTRACTION
-		{"900 converts to CM", 900, "CM"},
-		{"994 converts to CMXCIV", 994, "CMXCIV"},
-		{"1000 converts to M", 1000, "M"},
-		{"3999 converts to MMMCMXCIX", 3999, "MMMCMXCIX"},
-		// 4k and beyond requires new symbol for 'large 4k'
+		// check backwards from 1000
+		{Arabic: 999, Roman: "CMXCIX"},
+		{Arabic: 895, Roman: "DCCCXCV"},
+		{Arabic: 749, Roman: "DCCXLIX"},
+		{Arabic: 694, Roman: "DCXCIV"},
+		{Arabic: 544, Roman: "DXLIV"},
+		{Arabic: 499, Roman: "CDXCIX"},
+		{Arabic: 440, Roman: "CDXL"},
+		{Arabic: 199, Roman: "CXCIX"},
+		{Arabic: 99, Roman: "XCIX"},
+		{Arabic: 94, Roman: "XCIV"},
+		{Arabic: 49, Roman: "XLIX"},
+		{Arabic: 40, Roman: "XL"},
+		{Arabic: 9, Roman: "IX"},
+		{Arabic: 5, Roman: "V"},
+		{Arabic: 4, Roman: "IV"},
+		{Arabic: 3, Roman: "III"},
+		{Arabic: 2, Roman: "II"},
+		{Arabic: 1, Roman: "I"},
 	}
-
 	for _, test := range cases {
-		t.Run(test.Description, func(t *testing.T) {
+		t.Run(fmt.Sprintf("%d gets converted to %q", test.Arabic, test.Roman), func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
-			if got != test.Want {
-				t.Errorf("got %q, want %q", got, test.Want)
+			if got != test.Roman {
+				t.Errorf("got arabic: %d %q, want %q", test.Arabic, got, test.Roman)
+			}
+		})
+	}
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%q gets converted to %d", test.Roman, test.Arabic), func(t *testing.T) {
+			got := ConvertToArabic(test.Roman)
+			if got != test.Arabic {
+				t.Errorf("got roman: %q %d, want %d", test.Roman, got, test.Arabic)
 			}
 		})
 	}
 }
 
-func TestRomanNumeralsBigEndian(t *testing.T) {
-	cases := []struct {
-		Arabic    int
-		Assertion string
-		Want      string
-	}{
-		// addition through the symbols
-		{1000, "converts to M", "M"},
-		{2000, "converts to MM", "MM"},
-		{3000, "converts to MMM", "MMM"},
-		{3500, "converts to MMMD", "MMMD"},
-		{3600, "converts to MMMDC", "MMMDC"},
-		{3700, "converts to MMMDCC", "MMMDCC"},
-		{3800, "converts to MMMDCCC", "MMMDCCC"},
-		{3850, "converts to MMMDCCCL", "MMMDCCCL"},
-		{3900, "converts to MMMCM", "MMMCM"},
-		{3910, "converts to MMMCMX", "MMMCMX"},
-		{3920, "converts to MMMCMXX", "MMMCMXX"},
-		{3930, "converts to MMMCMXXX", "MMMCMXXX"},
-		{3935, "converts to MMMCMXXXV", "MMMCMXXXV"},
-		{3936, "converts to MMMCMXXXVI", "MMMCMXXXVI"},
-		{3938, "converts to MMMCMXXXVIII", "MMMCMXXXVIII"},
-		// time to bring in subtraction
-		{3400, "converts to MMMCD", "MMMCD"},
-		{3440, "converts to MMMCDXL", "MMMCDXL"},
-		{3444, "converts to MMMCDXLIV", "MMMCDXLIV"},
-		// did we do it?  Let's PLAY!
-		{3442, "converts to MMMCDXLII", "MMMCDXLII"},
-		{3441, "converts to MMMCDXLI", "MMMCDXLI"},
-		// let is begin again near the lost trail of 3444
-		// now that subtraction is implemented lets lean back into the bigendian thing
-		// and continue toward 3999 on the 4s and 9s
-		{3990, "converts to MMMCMXC", "MMMCMXC"},
-		{3994, "converts to MMMCMXCIV", "MMMCMXCIV"},
-		{3999, "converts to MMMCMXCIX", "MMMCMXCIX"},
-
-		// check backwards from 1000
-		{999, "converts to CMXCIX", "CMXCIX"},
-		{895, "converts to DCCCXCV", "DCCCXCV"},
-		{749, "converts to DCCXLIX", "DCCXLIX"},
-		{694, "converts to DCXCIV", "DCXCIV"},
-		{544, "converts to DXLIV", "DXLIV"},
-		// {3940, "converts to MMMCMXL", "MMMCMXL"},
-		// {3944, "converts to MMMCMXLIV", "MMMCMXLIV"},
-		// {3945, "converts to MMMCMXLV", "MMMCMXLV"},
-		// {3949, "converts to MMMCMXLIX", "MMMCMXLIX"},
-		// dare we try the TIPPY TOP now that subrtaction has been (hopefully) wroked in?
-		// yes, yes we should (documention, no, no we should not have, we ended up back at 3444)
-		// {3999, "converts to MMMCMXCIX", "MMMCMXCIX"},
-		// predictably borkied 😂
+// testing/quick will point out weaknesses in our code with
+// randomized data to fit the function signatures
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		if arabic < 0 || arabic > 3999 {
+			log.Println("arabic out of range", arabic)
+			return true
+		}
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
 	}
-	for _, test := range cases {
-		t.Run(test.Assertion, func(t *testing.T) {
-			got := ConvertToRomanBigEndian(test.Arabic)
-			if got != test.Want {
-				t.Errorf("got arabic: %d %q, want %q", test.Arabic, got, test.Want)
-			}
-		})
+	if err := quick.Check(
+		assertion, &quick.Config{MaxCount: 1000}); err != nil {
+		t.Error("failed checks", err)
 	}
+	// passing nil as default config
+	// if err := quick.Check(assertion, nil); err != nil {
+	// 	t.Error("failed checks", err)
+	// }
 }
