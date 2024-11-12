dc1b8bb3f86f739f90aeccacda7277d9d9bbe0fa
Joseph Myers <josephlmyers@gmail.com>
2024-11-12 12:34:43 -0600

    refactored to generic and commented for historical prosperity

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -65,103 +65,6 @@ func TestGenericStack(t *testing.T) {
 	})
 }
 
-func TestStackOfInts(t *testing.T) {
-	t.Run("empty stack", func(t *testing.T) {
-		s := new(StackOfInts)
-		got := s.IsEmpty()
-		want := true
-		AssertEqual(t, got, want)
-
-	})
-	t.Run("push stack", func(t *testing.T) {
-		s := new(StackOfInts)
-		AssertEqual(t, s.IsEmpty(), true)
-		s.Push(1)
-		AssertEqual(t, s.IsEmpty(), false)
-
-	})
-	t.Run("peek stack", func(t *testing.T) {
-		s := new(StackOfInts)
-		s.Push(1)
-		got, _ := s.Peek()
-		AssertEqual(t, got, 1)
-	})
-	t.Run("peek empty stack", func(t *testing.T) {
-		s := new(StackOfInts)
-		_, err := s.Peek()
-		if err == nil {
-			t.Errorf("expected error thrown")
-		}
-		AssertEqual(t, err.Error(), "cannot peek empty stack")
-	})
-	t.Run("pop stack", func(t *testing.T) {
-		s := new(StackOfInts)
-		s.Push(1)
-		s.Push(2)
-		got, _ := s.Pop()
-		AssertEqual(t, got, 2)
-		AssertEqual(t, s.IsEmpty(), false)
-	})
-	t.Run("pop empty stack throws error", func(t *testing.T) {
-		s := new(StackOfInts)
-		_, err := s.Pop()
-		if err == nil {
-			t.Errorf("expected error but got none")
-		}
-		AssertEqual(t, err.Error(), "cannot pop empty stack")
-	})
-}
-
-func TestStackOfStrings(t *testing.T) {
-	t.Run("empty stack", func(t *testing.T) {
-		s := new(StackOfStrings)
-		if !s.IsEmpty() {
-			t.Errorf("expected empty stack")
-		}
-	})
-	t.Run("push stack", func(t *testing.T) {
-		s := new(StackOfStrings)
-		s.Push("a")
-		AssertEqual(t, s.IsEmpty(), false)
-	})
-	t.Run("peek stack", func(t *testing.T) {
-		s := new(StackOfStrings)
-		s.Push("a")
-		got, _ := s.Peek()
-		if got != "a" {
-			t.Errorf("got %s, want a", got)
-		}
-	})
-	t.Run("pop empty stack throws error", func(t *testing.T) {
-		s := new(StackOfStrings)
-		_, err := s.Pop()
-		if err == nil {
-			t.Errorf("expected error thrown")
-		}
-		AssertEqual(t, err.Error(), "cannot pop empty stack")
-
-	})
-	t.Run("peek empty stack throws error", func(t *testing.T) {
-		s := new(StackOfStrings)
-		_, err := s.Peek()
-		if err == nil {
-			t.Errorf("expected error thrown")
-		}
-		AssertEqual(t, err.Error(), "cannot peek empty stack")
-	})
-}
-
-func TestAssertFunctions(t *testing.T) {
-	t.Run("asserting on integers", func(t *testing.T) {
-		AssertEqual(t, 1, 1)
-		AssertNotEqual(t, 1, 2)
-	})
-	t.Run("asserting on strings", func(t *testing.T) {
-		AssertEqual(t, "a", "a")
-		AssertNotEqual(t, "a", "b")
-	})
-}
-
 func AssertEqual[T comparable](t *testing.T, got, want T) {
 	t.Helper()
 	if got != want {
@@ -169,9 +72,89 @@ func AssertEqual[T comparable](t *testing.T, got, want T) {
 	}
 }
 
-func AssertNotEqual[T comparable](t *testing.T, got, want T) {
-	t.Helper()
-	if got == want {
-		t.Errorf("did not want matching values %+v & %+v", got, want)
-	}
-}
+// refactor to Generics boyeeeeee!
+// func TestStackOfInts(t *testing.T) {
+// 	t.Run("empty stack", func(t *testing.T) {
+// 		s := new(StackOfInts)
+// 		got := s.IsEmpty()
+// 		want := true
+// 		AssertEqual(t, got, want)
+//
+// 	})
+// 	t.Run("push stack", func(t *testing.T) {
+// 		s := new(StackOfInts)
+// 		AssertEqual(t, s.IsEmpty(), true)
+// 		s.Push(1)
+// 		AssertEqual(t, s.IsEmpty(), false)
+//
+// 	})
+// 	t.Run("peek stack", func(t *testing.T) {
+// 		s := new(StackOfInts)
+// 		s.Push(1)
+// 		got, _ := s.Peek()
+// 		AssertEqual(t, got, 1)
+// 	})
+// 	t.Run("peek empty stack", func(t *testing.T) {
+// 		s := new(StackOfInts)
+// 		_, err := s.Peek()
+// 		if err == nil {
+// 			t.Errorf("expected error thrown")
+// 		}
+// 		AssertEqual(t, err.Error(), "cannot peek empty stack")
+// 	})
+// 	t.Run("pop stack", func(t *testing.T) {
+// 		s := new(StackOfInts)
+// 		s.Push(1)
+// 		s.Push(2)
+// 		got, _ := s.Pop()
+// 		AssertEqual(t, got, 2)
+// 		AssertEqual(t, s.IsEmpty(), false)
+// 	})
+// 	t.Run("pop empty stack throws error", func(t *testing.T) {
+// 		s := new(StackOfInts)
+// 		_, err := s.Pop()
+// 		if err == nil {
+// 			t.Errorf("expected error but got none")
+// 		}
+// 		AssertEqual(t, err.Error(), "cannot pop empty stack")
+// 	})
+// }
+//
+// func TestStackOfStrings(t *testing.T) {
+// 	t.Run("empty stack", func(t *testing.T) {
+// 		s := new(StackOfStrings)
+// 		if !s.IsEmpty() {
+// 			t.Errorf("expected empty stack")
+// 		}
+// 	})
+// 	t.Run("push stack", func(t *testing.T) {
+// 		s := new(StackOfStrings)
+// 		s.Push("a")
+// 		AssertEqual(t, s.IsEmpty(), false)
+// 	})
+// 	t.Run("peek stack", func(t *testing.T) {
+// 		s := new(StackOfStrings)
+// 		s.Push("a")
+// 		got, _ := s.Peek()
+// 		if got != "a" {
+// 			t.Errorf("got %s, want a", got)
+// 		}
+// 	})
+// 	t.Run("pop empty stack throws error", func(t *testing.T) {
+// 		s := new(StackOfStrings)
+// 		_, err := s.Pop()
+// 		if err == nil {
+// 			t.Errorf("expected error thrown")
+// 		}
+// 		AssertEqual(t, err.Error(), "cannot pop empty stack")
+//
+// 	})
+// 	t.Run("peek empty stack throws error", func(t *testing.T) {
+// 		s := new(StackOfStrings)
+// 		_, err := s.Peek()
+// 		if err == nil {
+// 			t.Errorf("expected error thrown")
+// 		}
+// 		AssertEqual(t, err.Error(), "cannot peek empty stack")
+// 	})
+// }
