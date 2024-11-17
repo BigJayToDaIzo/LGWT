f96843175b5b8a1a27090071bd6b839c5f01b0c0
Joseph Myers <josephlmyers@gmail.com>
2024-11-17 10:30:48 -0600

    capitalized Generics directory name and abstracted logic from http handler

--- ScalingAcceptanceTests/handler.go
+++ ScalingAcceptanceTests/handler.go
@@ -8,5 +8,5 @@ import (
 func Handler(writer http.ResponseWriter, request *http.Request) {
 	// get name from request
 	name := request.URL.Query().Get("name")
-	fmt.Fprintf(writer, "Hello, %s", name)
+	fmt.Fprint(writer, Greet(name))
 }
